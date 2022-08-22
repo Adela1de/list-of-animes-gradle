9da3aacc429639bc2e56a782465755f15239c5e4
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-08-22 16:50:45 -0300

    Recreating the project this time with gradle

---
+++ user-handler-for-list-of-animes-project-remake-gradle/src/main/java/luiz/augusto/userhandlerforlistofanimesproject/UserHandlerForListOfAnimesProjectRemakeGradleApplication.java
@@ -0,0 +1,13 @@
+package luiz.augusto.userhandlerforlistofanimesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHandlerForListOfAnimesProjectRemakeGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHandlerForListOfAnimesProjectRemakeGradleApplication.class, args);
+	}
+
+}
